2fa43f181d1d1dfb2e31b749788af1604592ac57
Nandhini <nandhini.pandiarajan2000@gmail.com>
2021-10-29 19:13:21 +0530

    First commit

---
+++ SimpleExample/src/addValues/Main.java
@@ -0,0 +1,12 @@
+package addValues;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a= 5;
+       int b= 10;
+       System.out.println(a+b);
+	}
+
+}
